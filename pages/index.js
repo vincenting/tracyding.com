7f71b8a8940c3fdd3bef85295be1fe50c5f73111
Vincent Ting <1445522+vincenting@users.noreply.github.com>
2022-10-15 21:48:09 +0800

    feat: add the basic files for next.js

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Tracy Ding's Personal Site</title>
+      </Head>
+
+      <main>
+        Coming Soon
+      </main>
+    </div>
+  )
+}
