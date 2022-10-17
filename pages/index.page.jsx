1476d76b7479bac478517a67960de9bfcc53e063
Vincent Ting <1445522+vincenting@users.noreply.github.com>
2022-10-17 21:58:19 +0800

    feat: implement new coming soon page

--- pages/index.page.jsx
+++ pages/index.page.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 export default function Home () {
+  const audio = useRef()
+  useEffect(() => {
+    audio.current = new Audio('/audio/hi_im_tracy.mp3')
+  }, [])
+
+  const playHelloFromTracy = () => {
+    if (audio.current) {
+      audio.current.currentTime = 0
+      audio.current.play()
+    }
+  }
+
   return (
-    <main className="container max-w-md mx-auto overflow-hidden md:max-w-2xl mt-3">
-      <h3 className='indent-3'>
-        Dear Visitors, I&apos;m Tracy. 😊
-      </h3>
-      <p className='indent-3'>My Portfolio Website is coming soon. </p>
+    <main className="w-screen h-screen overflow-hidden flex bg-orange-100">
+      <div className='w-fit h-48 self-center ml-auto mr-auto container'>
+        <h3 className='text-lg text-center'>
+          Dear visitor,
+          I&apos;m <span onClick={playHelloFromTracy} className='text-pink-500 underline cursor-pointer'>Tracy.</span> 😊
+        </h3>
+        <p className='mt-2'>My Portfolio Website is coming soon. </p>
+      </div>
     </main>
   )
 }
